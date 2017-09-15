c2d3e1cd46baeddb150b048151a6ff7f8f6b494e
Kiran Gangadharan <me@kirang.in>
2017-09-15 11:40:07 +0530

    Use stable version of clojure

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]]
 
   :plugins [[lein-figwheel "0.5.13"]
